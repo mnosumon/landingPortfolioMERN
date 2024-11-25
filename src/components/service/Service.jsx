385ce73934d64a327484369f2cc3e2fc8b38f6eb
mnosumon <mdnuruddin378350@gmail.com>
2024-11-25 21:39:11 +0600

    service part designe done

--- src/components/service/Service.jsx
+++ src/components/service/Service.jsx
@@ -1,13 +1,26 @@
 import React from "react";
 import "./service.css";
+import Icon1 from "../../assets/icon/icon1.png";
+import Icon2 from "../../assets/icon/icon2.png";
+import Icon3 from "../../assets/icon/icon3.png";
+import Icon4 from "../../assets/icon/icon4.png";
+import Icon5 from "../../assets/icon/icon5.png";
+import Icon6 from "../../assets/icon/icon6.png";
+import Icon7 from "../../assets/icon/icon7.png";
+import Icon8 from "../../assets/icon/icon8.png";
+import FrameIcon from "../../assets/icon/frameIcon.png";
 
 const Service = () => {
   return (
-    <section className="services">
-      <h1>Services I Offer</h1>
+    <section className="services container">
+      <div className="textCenter">
+        <img className="faramIcon" src={FrameIcon} alt="Frame Icon" />
+        <span className="subtitle">I AM DESIGNER</span>
+      </div>
+      <h1 className="serviceHead">Services I Offer</h1>
       <div className="services-grid">
         <div className="service">
-          <img src="icons/uiux-icon.png" alt="UI/UX Design Icon" />
+          <img src={Icon2} alt="UI/UX Design Icon" />
           <h2>UI/UX Design</h2>
           <p>
             When our power of choice is untrammelled and nothing prevents our
@@ -15,7 +28,7 @@ const Service = () => {
           </p>
         </div>
         <div className="service">
-          <img src="icons/mobile-icon.png" alt="Mobile App Icon" />
+          <img src={Icon1} alt="Mobile App Icon" />
           <h2>Mobile App</h2>
           <p>
             When our power of choice is untrammelled and nothing prevents our
@@ -23,7 +36,7 @@ const Service = () => {
           </p>
         </div>
         <div className="service">
-          <img src="icons/graphic-icon.png" alt="Graphic Design Icon" />
+          <img src={Icon8} alt="Graphic Design Icon" />
           <h2>Graphic Design</h2>
           <p>
             When our power of choice is untrammelled and nothing prevents our
@@ -31,7 +44,7 @@ const Service = () => {
           </p>
         </div>
         <div className="service">
-          <img src="icons/webdev-icon.png" alt="Web Developer Icon" />
+          <img src={Icon3} alt="Web Developer Icon" />
           <h2>Web Developer</h2>
           <p>
             When our power of choice is untrammelled and nothing prevents our
@@ -39,7 +52,7 @@ const Service = () => {
           </p>
         </div>
         <div className="service">
-          <img src="icons/seo-icon.png" alt="SEO Optimization Icon" />
+          <img src={Icon7} alt="SEO Optimization Icon" />
           <h2>SEO Optimization</h2>
           <p>
             When our power of choice is untrammelled and nothing prevents our
@@ -47,7 +60,7 @@ const Service = () => {
           </p>
         </div>
         <div className="service">
-          <img src="icons/wordpress-icon.png" alt="WordPress Developer Icon" />
+          <img src={Icon6} alt="WordPress Developer Icon" />
           <h2>WordPress Developer</h2>
           <p>
             When our power of choice is untrammelled and nothing prevents our
@@ -55,7 +68,7 @@ const Service = () => {
           </p>
         </div>
         <div className="service">
-          <img src="icons/appdev-icon.png" alt="App Development Icon" />
+          <img src={Icon4} alt="App Development Icon" />
           <h2>App Development</h2>
           <p>
             When our power of choice is untrammelled and nothing prevents our
@@ -63,7 +76,7 @@ const Service = () => {
           </p>
         </div>
         <div className="service">
-          <img src="icons/business-icon.png" alt="Business Strategy Icon" />
+          <img src={Icon5} alt="Business Strategy Icon" />
           <h2>Business Strategy</h2>
           <p>
             When our power of choice is untrammelled and nothing prevents our
