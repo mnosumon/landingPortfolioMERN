d03830380ab7036836c90e2173611070e9ecec05
mnosumon <mdnuruddin378350@gmail.com>
2024-11-26 17:02:07 +0600

    Partner  done

--- src/components/partner/Partner.jsx
+++ src/components/partner/Partner.jsx
@@ -1,27 +1,43 @@
 import React from "react";
+import "./partner.css";
+import FrameIcon from "../../assets/icon/frameIcon.png";
+import Frame01 from "../../assets/icon/frame01.png";
+import Frame02 from "../../assets/icon/frame02.png";
+import Frame03 from "../../assets/icon/frame03.png";
+import Frame04 from "../../assets/icon/frame04.png";
+import Frame05 from "../../assets/icon/frame05.png";
 
 const Partner = () => {
   return (
     <div class="partners-section">
-      <h2>REPUTED PARTNER</h2>
-      <div class="partners">
-        <div class="partner">
-          <img src="logo1.jpg" alt="Partner Logo" />
+      <div className="container">
+        <div className="textCenter">
+          <img className="faramIcon" src={FrameIcon} alt="Frame Icon" />
+          <span className="subtitle">My partner</span>
         </div>
-        <div class="partner">
-          <img src="logo2.jpg" alt="Partner Logo" />
-        </div>
-        <div class="partner">
-          <img src="logo3.jpg" alt="Partner Logo" />
-        </div>
-        <div class="partner">
-          <img src="logo4.jpg" alt="Partner Logo" />
-        </div>
-        <div class="partner">
-          <img src="logo5.jpg" alt="Partner Logo" />
-        </div>
-        <div class="partner">
-          <img src="logo6.jpg" alt="Partner Logo" />
+        <h2 className="contactHeading">REPUTED PARTNER</h2>
+        <div className="partnerGrid">
+          <div className="partner">
+            <img src={Frame01} alt="Graphic Design Icon" />
+          </div>
+          <div className="partner">
+            <img src={Frame01} alt="Graphic Design Icon" />
+          </div>
+          <div className="partner">
+            <img src={Frame02} alt="Graphic Design Icon" />
+          </div>
+          <div className="partner">
+            <img src={Frame03} alt="Graphic Design Icon" />
+          </div>
+          <div className="partner">
+            <img src={Frame04} alt="Graphic Design Icon" />
+          </div>
+          <div className="partner">
+            <img src={Frame05} alt="Graphic Design Icon" />
+          </div>
+          <div className="partner">
+            <img src={Frame01} alt="Graphic Design Icon" />
+          </div>
         </div>
       </div>
     </div>
